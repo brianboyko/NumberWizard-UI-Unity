32332eff87f618b9a47b98ea55d4c908ca8326ec
Brian Boyko <brian.boyko@gmail.com>
2016-01-11 04:08:17 -0600

    first commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadLevel(string name){
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void quitRequest(){
+		Debug.Log ("Game quit in progress");
+		Application.Quit ();
+	}
+
+
+}
